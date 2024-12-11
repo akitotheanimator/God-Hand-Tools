bc12726296b81106f7073d4755d89aed365f4646
akitotheanimator <sapressaonenem@gmail.com>
2024-12-11 20:36:55 -0300

    Add files via upload

---
+++ MOTool/MOTool/GetMot.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.IO;
+
+
+
+
+
+public static class GetMot
+{
+    public static void processMot(string file)
+    {
+        if(!Directory.Exists(file.Replace(Path.GetFileName(file), "") + "/MOTIONS_"))
+            Directory.CreateDirectory(file.Replace(Path.GetFileName(file),"") + "/MOTIONS_");
+
+        Program.saveFileTo = file.Replace(Path.GetFileName(file), "") + "/MOTIONS_";
+
+
+
+        GlobalTools.FilePath = Program.pathForBones;
+
+        List<BONES> bnl = new List<BONES>();
+        BONES root = new BONES();
+        root.pos = new Vector3(0,0,0);
+        //bnl.Add(root);
+        using (FileStream fs = new FileStream(Program.pathForBones, FileMode.Open))
+        using (BinaryReader br = new BinaryReader(fs))
+        {
+
+            for (int i = 0; i < fs.Length; i += 16)
+            {
+                BONES bn = new BONES();
+                bn.pos = new Vector3(GlobalTools.readValue(fs, br, i, GlobalTools.NumberType.SINGLE), GlobalTools.readValue(fs, br, i + 4, GlobalTools.NumberType.SINGLE), GlobalTools.readValue(fs, br, i + 8, GlobalTools.NumberType.SINGLE));
+                bn.parentingOrder = GlobalTools.readValue(fs, br, i + 12, GlobalTools.NumberType.SHORT);
+                bn.id = GlobalTools.readValue(fs, br, i + 14, GlobalTools.NumberType.SHORT);
+                bnl.Add(bn);
+                //Console.WriteLine(x + "  " + y + "  " + z + "  " + id2);
+            }
+        }
+
+        Program.bones_ = bnl.ToArray();
+        Program.bones_[1].pos = new Vector3(0,0,0);
+
+
+        GlobalTools.FilePath = file;
+        MOTFile.bones = new MOTBone[0];
+
+
+
+
+        GlobalTools.changeColor(ConsoleColor.Green);
+        Console.WriteLine("This is a valid MOT File!");
+        GlobalTools.changeColor(ConsoleColor.Yellow);
+        Console.WriteLine("Reading Header");
+
+        using (FileStream fs = new FileStream(file, FileMode.Open))
+        using (BinaryReader br = new BinaryReader(fs))
+        {
+
+            #region read header start
+            ushort FrameCount = GlobalTools.readValue(fs, br, 4, GlobalTools.NumberType.USHORT);
+            byte countOfKeyframedproperties = GlobalTools.readValue(fs, br, 6, GlobalTools.NumberType.BYTE);
+            byte UseLoop = GlobalTools.readValue(fs, br, 7, GlobalTools.NumberType.BYTE);
+            fs.Position = 8;
+            GlobalTools.changeColor(ConsoleColor.Green);
+            GlobalTools.cleanSection($"Animation frame count: {FrameCount}, Keyframed components: {countOfKeyframedproperties}");
+            #endregion
+
+            GlobalTools.changeColor(ConsoleColor.Yellow);
+            Console.WriteLine("Reading Header Data...");
+            int propertyEndOffset = (countOfKeyframedproperties * 12) + 8;
+            int tempCount = 0;
+            GlobalTools.changeColor(ConsoleColor.Blue);
+
+            for (int i = 8; i < propertyEndOffset; i += 12)
+            {
+
+                MOTBone bone = new MOTBone();
+                MOTHeader keyframe = new MOTHeader();
+                if (GlobalTools.readValue(fs, br, i + 8, GlobalTools.NumberType.UINT) != 0 && GlobalTools.readValue(fs, br, i + 8, GlobalTools.NumberType.UINT) < fs.Length && GlobalTools.readValue(fs, br, i, GlobalTools.NumberType.UBYTE) != 127)
+                {
+                    bone.index = GlobalTools.readValue(fs, br, i, GlobalTools.NumberType.UBYTE);
+                    bone.type = GlobalTools.getType(GlobalTools.readValue(fs, br, i + 1, GlobalTools.NumberType.BYTE));
+                    bone.typeOriginal = GlobalTools.readValue(fs, br, i + 1, GlobalTools.NumberType.BYTE);
+                    keyframe.keyframeCount = GlobalTools.readValue(fs, br, i + 2, GlobalTools.NumberType.USHORT);
+                    keyframe.isAbsoluteCoordinate = GlobalTools.readValue(fs, br, i + 4, GlobalTools.NumberType.UINT) == 1;
+                    keyframe.adress = GlobalTools.readValue(fs, br, i + 8, GlobalTools.NumberType.UINT);
+                    Console.WriteLine(bone.type + " " + bone.index + " " + bone.typeOriginal);
+                    //Console.WriteLine(GlobalTools.readValue(fs, br, i + 4, GlobalTools.NumberType.INT));
+
+                    bone.data = keyframe;
+                    MOTFile.addBone(bone);
+
+                    #region console stuff
+                    if(1>3)
+                    if (tempCount == 0)
+                        Console.WriteLine($"Bone index: {bone.index}   (Keyframe Type: {bone.type}, Total Keyframes on property: {keyframe.keyframeCount} MV{keyframe.isAbsoluteCoordinate}, localized at: 0x{keyframe.adress.ToString("X")})");
+                    else
+                        Console.WriteLine($"Bone index: {bone.index}   (Keyframe Type: {bone.type}, Total Keyframes on property: {keyframe.keyframeCount} MV{keyframe.isAbsoluteCoordinate}, localized at: 0x{keyframe.adress.ToString("X")} ({tempCount}))");
+                    tempCount += 1;
+
+                    //Console.WriteLine(i);
+                    #endregion
+                }
+            }
+            //Thread.Sleep(15);
+            GlobalTools.changeColor(ConsoleColor.Green);
+            Console.WriteLine("Done!");
+            GlobalTools.changeColor(ConsoleColor.Yellow);
+            Console.WriteLine("Reading MOT data...");
+            for (int i = 0; i < MOTFile.bones.Length; i++)
+            {
+                if (MOTFile.bones[i].typeOriginal < 80)
+                {
+                    int additive = 0;
+                    MOTFile.bones[i].data.P = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 0, GlobalTools.NumberType.HALF);
+                    MOTFile.bones[i].data.Dp = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 2, GlobalTools.NumberType.HALF);
+                    MOTFile.bones[i].data.M0 = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 4, GlobalTools.NumberType.HALF);
+                    MOTFile.bones[i].data.Dm0 = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 6, GlobalTools.NumberType.HALF);
+                    MOTFile.bones[i].data.M1 = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 8, GlobalTools.NumberType.HALF);
+                    MOTFile.bones[i].data.Dm1 = (float)GlobalTools.readValue(fs, br, MOTFile.bones[i].data.adress + 10, GlobalTools.NumberType.HALF);
+                    for (uint a = MOTFile.bones[i].data.adress + 12; a < (((MOTFile.bones[i].data.adress + (MOTFile.bones[i].data.keyframeCount * 4) + 12))); a += 4)
+                    {
+                        if (a == fs.Length) break;
+                        MOTKeyframe cv = new MOTKeyframe(); //curve
+                        additive += GlobalTools.readValue(fs, br, a, GlobalTools.NumberType.BYTE);
+                        cv.absolute_index = additive;
+
+
+
+                        cv.Cp = GlobalTools.readValue(fs, br, a + 1, GlobalTools.NumberType.BYTE);
+                        cv.Cm0 = GlobalTools.readValue(fs, br, a + 2, GlobalTools.NumberType.BYTE);
+                        cv.Cm1 = GlobalTools.readValue(fs, br, a + 3, GlobalTools.NumberType.BYTE);
+                        MOTFile.addCurve(cv, MOTFile.bones[i].data);
+                    }
+                } else
+                {
+                    for (uint a = MOTFile.bones[i].data.adress; a < (((MOTFile.bones[i].data.adress + (MOTFile.bones[i].data.keyframeCount * 16)))); a += 16)
+                    {
+                        if (a == fs.Length) break;
+                        MOTKeyframe cv = new MOTKeyframe(); //curve
+                        cv.absolute_index = GlobalTools.readValue(fs, br, a, GlobalTools.NumberType.USHORT);
+                        cv.P0 = GlobalTools.readValue(fs, br, a + 4, GlobalTools.NumberType.SINGLE);
+                        cv.M0 = GlobalTools.readValue(fs, br, a + 8, GlobalTools.NumberType.SINGLE);
+                        cv.M1 = GlobalTools.readValue(fs, br, a + 12, GlobalTools.NumberType.SINGLE);
+                        Console.WriteLine(a + " " + cv.absolute_index + " " + MOTFile.bones[i].index + "   " + cv.P0);
+                        MOTFile.addCurve(cv, MOTFile.bones[i].data);
+                    }
+                }
+            }
+
+
+
+            int greatest = 0;
+            for (int i = 0; i < MOTFile.bones.Length; i++)
+            {
+                if (MOTFile.bones[i].data.curves.Length > greatest) greatest = MOTFile.bones[i].data.curves.Length;
+            }
+            for (int i = 0; i < MOTFile.bones.Length; i++)
+            {
+                List<MOTKeyframe> cvs = MOTFile.bones[i].data.curves.ToList();
+                if (MOTFile.bones[i].data.curves.Length < greatest)
+                {
+                    for (int o = MOTFile.bones[i].data.curves.Length; o < greatest; o++)
+                    {
+                        cvs.Add(cvs[cvs.Count - 1]);
+                    }
+                }
+                if (cvs.Count > 0) MOTFile.bones[i].data.curves = cvs.ToArray();
+            }
+        }
+        GlobalTools.changeColor(ConsoleColor.Green);
+        Console.WriteLine("Done!");
+        GlobalTools.changeColor(ConsoleColor.Red);
+        Console.WriteLine("Quantizing Values...");
+        MOTFile.Quantize();
+        GlobalTools.changeColor(ConsoleColor.Green);
+        Console.WriteLine("Done!");
+        SMDExportMOT.Export(file);
+    }
+}
---
+++ MOTool/MOTool/constructor/MOT.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class MOTBone
+{
+    public sbyte index;
+    public byte typeOriginal;
+    public string type;
+    public MOTHeader data;
+}
+public class MOTKeyframe
+{
+    public int absolute_index; //absolute index
+    public byte Cp;
+    public byte Cm0;
+    public byte Cm1;
+
+
+
+    public float P0; //when the file uses full values instead of quantized ones
+    public float M0;
+    public float M1;
+}
+
+public class MOTHeader
+{
+    public uint adress;
+    public ushort keyframeCount;
+    public bool isAbsoluteCoordinate;
+
+
+    public float P;
+    public float Dp;
+    public float M0;
+    public float Dm0;
+    public float M1;
+    public float Dm1;
+    public MOTKeyframe[] curves;
+
+    public float[] Values;
+
+
+    public float Interpol(int frame, int i, bool isQuantized)
+    {
+        GlobalTools.changeColor(ConsoleColor.Blue);
+        float p0 = curves[i].P0;
+        float m0 = curves[i].M1;
+        float p1 = curves[i + 1].P0;
+        float m1 = curves[i + 1].M0;
+
+        if (isQuantized)
+        {
+            p0 = P + Dp * curves[i].Cp;
+            m0 = M1 + Dm1 * curves[i].Cm1;
+            p1 = P + Dp * curves[i + 1].Cp;
+            m1 = M0 + Dm0 * curves[i + 1].Cm0;
+        }
+
+
+        float t = (float)(frame - curves[i].absolute_index) / (curves[i + 1].absolute_index - curves[i].absolute_index);
+        if (t.ToString() == "NaN") t = 0f;
+
+        float ret = (float)(2 * Math.Pow(t,3) - 3 * Math.Pow(t, 2) + 1) * p0 + (float)(Math.Pow(t, 3) - 2 * Math.Pow(t, 2) + t) * m0 + (float)(-2 * Math.Pow(t, 3) + 3 * Math.Pow(t, 2)) * p1 + (float)(Math.Pow(t, 3) - Math.Pow(t, 2)) * m1;
+
+
+        List<float> floats = new List<float>();
+        floats.Add(ret);
+        if (Values == null)
+        {
+            Values = floats.ToArray();
+        }
+        else
+        {
+            floats = Values.ToList();
+            floats.Add(ret);
+            Values = floats.ToArray();
+        }
+        return ret;
+    }
+
+}
+
+
+
+public static class MOTFile
+{
+    public static void Quantize()
+    {
+
+        for (int a = 0; a < bones.Length; a++)
+        {
+            for (int i = 0; i < bones[a].data.curves.Length - 1; i++)
+            {
+                for (int frame = bones[a].data.curves[i].absolute_index; frame < bones[a].data.curves[i+1].absolute_index; frame++)
+                {
+                    float v = bones[a].data.Interpol(frame, i, bones[a].typeOriginal < 80);
+                }
+            }
+        }
+        
+    }
+
+    public static MOTBone[] bones;
+    public static void addBone(MOTBone b)
+    {
+        List<MOTBone> Bones = bones.ToList();
+        Bones.Add(b);
+        bones = Bones.ToArray();
+    }
+    public static int[] Distinct(int refi)
+    {
+        List<int> ind = new List<int>(); //will list every index that has the same property, which means, each one of them will list different actions
+        for (int i = 0; i < MOTFile.bones.Length; i++)
+        {
+            if (MOTFile.bones[i].index == refi) ind.Add(i);
+        }
+        return ind.ToArray();
+    }
+    public static bool isIK(int[] re)
+    {
+        bool ret = false;
+        for (int i = 0; i < re.Length; i++)
+            if (MOTFile.bones[re[i]].data.isAbsoluteCoordinate)
+                ret = true;
+
+        return ret;
+    }
+    public static int getFrameLength()
+    {
+        int rlength = -1;
+        for (int i = 0; i < bones.Length; i++)
+            if (bones[i] != null)
+                if (bones[i].data.Values != null)
+                    if (bones[i].data.Values.Length > 0)
+                        if (rlength < bones[i].data.Values.Length)
+                            rlength = bones[i].data.Values.Length;
+        return rlength;
+    }
+    public static void addCurve(MOTKeyframe c, MOTHeader k)
+    {
+        if (k.curves != null)
+        {
+            List<MOTKeyframe> Curves = k.curves.ToList();
+            Curves.Add(c);
+            k.curves = Curves.ToArray();
+        }
+        else
+        {
+            List<MOTKeyframe> Curves = new List<MOTKeyframe>();
+            Curves.Add(c);
+            k.curves = Curves.ToArray();
+        }
+    }
+}
